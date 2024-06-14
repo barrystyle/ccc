797de081434d6c62eb02836476002c7109c4ee92
Hser2bio <hser2bio@icloud.com>
2024-06-14 10:07:33 +0200

    Update version.h

--- src/version.h
+++ src/version.h
@@ -19,10 +19,8 @@ static const int PROTOCOL_VERSION = 70922;
 static const int INIT_PROTO_VERSION = 209;
 
 //! disconnect from peers older than this proto version
-//static const int MIN_PEER_PROTO_VERSION = 70920;
-//! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = PROTOCOL_VERSION;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = PROTOCOL_VERSION;
+static const int MIN_PEER_PROTO_VERSION = PROTOCOL_VERSION;
+
 
 //! In this version, 'getheaders' was introduced.
 static const int GETHEADERS_VERSION = 70077;
